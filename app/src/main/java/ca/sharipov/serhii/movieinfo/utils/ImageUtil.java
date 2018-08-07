8de044323bb51b63f2917bccfeec7414ec07dd74
Sergey-PC <sharipov14@gmail.com>
2018-08-06 20:24:37 -0400

    Updated versions of libraries

--- app/src/main/java/ca/sharipov/serhii/movieinfo/utils/ImageUtil.java
+++ app/src/main/java/ca/sharipov/serhii/movieinfo/utils/ImageUtil.java
@@ -4,12 +4,13 @@ import android.content.Context;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 
 public class ImageUtil {
     public static void loadImage(Context context, ImageView imageView, String url) {
         Glide.with(context)
                 .load(url)
-                .centerCrop()
+                .apply(RequestOptions.centerCropTransform())
                 .into(imageView);
     }
 
